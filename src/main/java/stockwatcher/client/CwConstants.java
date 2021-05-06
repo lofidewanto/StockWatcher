c0c31f3ee5ea535a2da87ed7b309d77c6af28bc1
anil <1dropaflame@gmail.com>
2021-05-06 13:38:49 -0500

    client side working

---
+++ src/main/java/stockwatcher/client/CwConstants.java
@@ -0,0 +1,26 @@
+package stockwatcher.client;
+
+import com.google.gwt.i18n.client.Constants;
+
+/**
+ * The constants used in this Content Widget.
+ */
+public interface CwConstants extends Constants {
+    @DefaultStringValue("FileUpload")
+    String cwFileUploadButton();
+
+    @DefaultStringValue("Upload")
+    String cwFileUploadDescription();
+
+    @DefaultStringValue("FileUploadName")
+    String cwFileUploadName();
+
+    @DefaultStringValue("No File")
+    String cwFileUploadNoFileError();
+
+    @DefaultStringValue("Select the File")
+    String cwFileUploadSelectFile();
+
+    @DefaultStringValue("Success!!!")
+    String cwFileUploadSuccessful();
+}
---
+++ src/main/java/stockwatcher/client/StockWatcher.java
@@ -0,0 +1,20 @@
+package stockwatcher.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.*;
+
+public class StockWatcher implements EntryPoint {
+    TabPanel tabPanel = new TabPanel ();
+    ResponseFromServerPanel responseFromServerPanel = new ResponseFromServerPanel();
+    FileUploadPanel fileUploadPanel = new FileUploadPanel();
+    /**
+     * Entry point method.
+     */
+    public void onModuleLoad() {
+        VerticalPanel stocksPanel = responseFromServerPanel.createMainPanel();
+        VerticalPanel uploadPanel = fileUploadPanel.createVerticalPanel();
+        tabPanel.add(uploadPanel, "file upload");
+        tabPanel.add(stocksPanel, "response from server");
+        RootPanel.get("stockList").add(tabPanel);
+    }
+}
