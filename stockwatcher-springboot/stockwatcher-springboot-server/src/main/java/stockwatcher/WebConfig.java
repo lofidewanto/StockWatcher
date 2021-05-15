5dad8093cf4f38bb9c9fecd58ff4413d06164f4d
lofidewanto <lofidewanto@gmail.com>
2021-05-16 01:02:22 +0200

    update

--- stockwatcher-springboot/stockwatcher-springboot-server/src/main/java/stockwatcher/WebConfig.java
+++ stockwatcher-springboot/stockwatcher-springboot-server/src/main/java/stockwatcher/WebConfig.java
@@ -1,37 +1,17 @@
 package stockwatcher;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import stockwatcher.server.StockPriceServiceImpl;
 
 @Configuration
 public class WebConfig {
-	
+
 	@Bean
 	public ServletRegistrationBean<StockPriceServiceImpl> servletRegistrationBean() {
 		return new ServletRegistrationBean<StockPriceServiceImpl>(new StockPriceServiceImpl(), "/stockwatcher" + "/*");
 	}
 
-	/*
-	@Bean
-	public FilterRegistrationBean<CorsFilter> corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("http://127.0.0.1");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
-		source.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
-		bean.setOrder(0);
-		
-		return bean;
-	}
-	*/
 }
