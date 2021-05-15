96ff63febf4799de67e76721f47e2d8dc0853fb6
lofidewanto <lofidewanto@gmail.com>
2021-05-15 23:43:58 +0200

    rename

--- stockwatcher-springboot/stockwatcher-springboot-server/src/main/java/stockwatcher/controller/StockPriceRestController.java
+++ stockwatcher-springboot/stockwatcher-springboot-server/src/main/java/stockwatcher/controller/StockPriceRestController.java
@@ -1,4 +1,4 @@
-package stockwatcher.rest;
+package stockwatcher.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
