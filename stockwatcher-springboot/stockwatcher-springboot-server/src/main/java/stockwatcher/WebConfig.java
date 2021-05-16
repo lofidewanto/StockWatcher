a55b8aaa831aff04fb9f740c0cfffb94b9b832c8
lofidewanto <lofidewanto@gmail.com>
2021-05-16 15:42:34 +0200

    update

--- stockwatcher-springboot/stockwatcher-springboot-server/src/main/java/stockwatcher/WebConfig.java
+++ stockwatcher-springboot/stockwatcher-springboot-server/src/main/java/stockwatcher/WebConfig.java
@@ -5,13 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import stockwatcher.server.StockPriceServiceImpl;
+import stockwatcher.shared.StockPriceServiceEndpoint;
 
 @Configuration
 public class WebConfig {
 
 	@Bean
 	public ServletRegistrationBean<StockPriceServiceImpl> servletRegistrationBean() {
-		return new ServletRegistrationBean<StockPriceServiceImpl>(new StockPriceServiceImpl(), "/stockwatcher/stockPrice" + "/*");
+		return new ServletRegistrationBean<StockPriceServiceImpl>(new StockPriceServiceImpl(),
+				StockPriceServiceEndpoint.SERVER_SEPARATOR + StockPriceServiceEndpoint.SERVER_CONTEXT_NAME
+						+ StockPriceServiceEndpoint.STOCK_PRICE_RPC + "/*");
 	}
 
 }
